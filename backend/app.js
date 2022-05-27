45a26921ac20fa6eb9673af69bc61399dfbb9866
Hygei <k.guy.35@gmail.com>
2022-05-27 22:56:36 +0200

    ajout du parcours utilsateur / route middleware controller et authentification

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,21 @@ mongoose.connect('mongodb+srv://ProYoxiS:26049335tr@cluster0.m5kcj.mongodb.net/?
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  
+  app.use(express.json());
 
 
 
